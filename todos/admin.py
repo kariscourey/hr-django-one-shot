b00e7be2ab2187c78749618e622bceefbbb4cb40
karis courey <kariscourey@gmail.com>
2022-09-07 12:18:32 -0400

    feat 3 complete

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from todos.models import TodoList
+
+
+class TodoListAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TodoList, TodoListAdmin)
